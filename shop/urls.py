aa083283262f57df36c27cd6b1f3d3adff1b5b06
dastanslam <dslam5956@gmail.com>
2026-01-19 17:55:10 +0500

    regu

--- shop/urls.py
+++ shop/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('shop/', views.shop, name='shop'),
+    path('product/', views.product, name='product'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello from app")
+    return render(request, "index.html")
+
+def shop(request):
+    return render(request, "shop.html")
+
+def product(request):
+    return render(request, "product.html")
+
