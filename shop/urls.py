fc379d051a9e0c7b20e47d8a1679fac15f4188e9
dastanslam <dslam5956@gmail.com>
2026-01-26 17:07:07 +0500

    regu

--- shop/urls.py
+++ shop/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('signin/', views.signin, name='signin'),
     path('signup/', views.signup, name='signup'),
+    path('about/', views.about, name='about'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -6,23 +6,25 @@ def index(request):
     return render(request, "index.html", {"store": request.store})
 
 def shop(request):
-    return render(request, "shop.html")
+    return render(request, "shop.html", {"store": request.store})
 
 def product(request):
-    return render(request, "product.html")
+    return render(request, "product.html", {"store": request.store})
 
 def cart(request):
-    return render(request, "cart.html")
+    return render(request, "cart.html", {"store": request.store})
 
 def whislist(request):
-    return render(request, "whislist.html")
+    return render(request, "whislist.html", {"store": request.store})
 
 def contact(request):
-    return render(request, "contact.html")
+    return render(request, "contact.html", {"store": request.store})
 
 def signin(request):
-    return render(request, "signin.html")
+    return render(request, "signin.html", {"store": request.store})
 
 def signup(request):
-    return render(request, "signup.html")
+    return render(request, "signup.html", {"store": request.store})
 
+def about(request):
+    return render(request, "about.html", {"store": request.store})
